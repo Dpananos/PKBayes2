1ed91a50d93f5f814af370316f6e41a841b8af1d
Demetri <dpananos@gmail.com>
2020-08-30 18:05:11 -0400

    unreasonably good results

---
+++ model_dev.R
@@ -0,0 +1,132 @@
+library(cmdstanr)
+library(tidyverse)
+library(tidybayes)  
+library(posterior)
+library(patchwork)
+library(bayesplot)
+
+num_cores <- parallel::detectCores()
+# ---- Helper function ----
+
+plot_random_effects<-function(draws, covar, random_effect){
+  
+  x<- enquo(covar)
+  y<- enquo(random_effect)
+  
+  draws %>% 
+    spread_draws((!!y)[i]) %>% 
+    mean_qi %>% 
+    left_join(subjects) %>% 
+    ggplot(aes(!!x, !!y, color = sex, ymin = .lower, ymax = .upper))+
+    geom_pointrange()+
+    geom_smooth(method = 'lm')
+}
+
+plot_rfx_grid<-function(draws, random_effect, title){
+  
+  y <- enquo(random_effect)
+  
+  a <- plot_random_effects(draws, weight, !!y)
+  b <- plot_random_effects(draws, age , !!y)
+  c <- plot_random_effects(draws, creatinine , !!y)
+  
+  (a + b)/(c + plot_spacer()) + plot_annotation(title = title)
+  
+}
+
+# ---- Rerun old model ----
+
+combined_data<-read_csv('data/combined_data.csv') %>% 
+               filter(study=='old') %>% 
+               mutate(yobs=yobs/1000, i = seq_along(subjectids))
+
+subjects<- distinct(combined_data, subjectids, .keep_all = T) %>% 
+            mutate(i=subjectids)
+
+model_1_data <- combined_data %>% 
+              select(subjectids, time, yobs, D) %>% 
+              compose_data(.n_name = n_prefix("N"))
+
+model_1_data$n_subjects <- n_distinct(combined_data$subjectids)
+
+model <- cmdstan_model(stan_file = 'model_dev_1.stan')
+
+fit <- model$sample(data=model_1_data, 
+                    parallel_chains=num_cores, 
+                    chains=num_cores)
+
+draws <- as_draws_df(fit$draws())
+
+plot_rfx_grid(draws, z_cl, 'Clearance')
+  
+# ---- Rerun old model, but include some covariate effects on 
+
+model_2_data <- combined_data %>% 
+  select(subjectids, time, yobs, D) %>% 
+  compose_data(.n_name = n_prefix("N"))
+
+model_2_data$n_subjects <- n_distinct(combined_data$subjectids)
+model_2_data$sex<- if_else(subjects$sex=='male',1,0)
+model_2_data$weight<- as.numeric(scale(subjects$weight))
+model_2_data$creatinine<- as.numeric(scale(subjects$creatinine))
+model_2_data$D<- subjects$D
+
+model <- cmdstan_model(stan_file = 'model_dev_2.stan')
+
+fit <- model$sample(data=model_2_data, parallel_chains=num_cores, chains=num_cores)
+
+draws <- as_draws_df(fit$draws())
+
+plot_rfx_grid(draws, z_cl, 'Clearance')
+plot_rfx_grid(draws, z_t, 'tmax')
+plot_rfx_grid(draws, z_alpha, 'alpha')
+
+mcmc_hist(fit$draws(), regex_pars = 'beta')
+
+# ---- Summarize priors ----
+
+draws %>% 
+  gather_draws(sigma) %>% 
+  pull(.value) %>% 
+  fitdistrplus::fitdist(., 'gamma') %>% plot
+
+
+# ---- New Data ----
+
+d<-read_csv('data/combined_data.csv')
+
+old = filter(d, study=='old')
+
+old_subjects<- distinct(old, subjectids, .keep_all = T)
+
+
+new = filter(d, study=='new') %>% 
+  mutate(subjectids =  factor(subjectids - min(subjectids)+1),
+         weight = (weight - mean(old_subjects$weight))/sd(old_subjects$weight),
+         age = (age - mean(old_subjects$age))/sd(old_subjects$age),
+         creatinine  = (creatinine  - mean(old_subjects$creatinine ))/sd(old_subjects$creatinine ),
+         sex = if_else(sex=='male', 1, 0),
+         yobs = yobs*0.001
+  ) %>% 
+  select(-study) 
+
+model_data = compose_data(new, .n_name=n_prefix('N'))
+
+model_data
+model = cmdstan_model(stan_file='model_dev_3.stan')
+
+
+fit = model$sample(model_data, chains = 4, parallel_chains = num_cores)
+
+fit$draws('C') %>% 
+  as_draws_df() %>% 
+  spread_draws(C[i]) %>% 
+  mean_qi %>% 
+  mutate(i = factor(i)) %>% 
+  left_join(new, by = c('i'='subjectids')) %>% 
+  ggplot(aes(yobs, C))+
+  geom_point(aes(ymin = .lower, ymax = .upper))+
+  geom_abline()+
+  labs(x='observed', y='predicted')
+
+
--- model_dev_2.stan
+++ model_dev_2.stan
@@ -1,29 +1,4 @@
-functions {
-  real[] one_comp_lin_elim_abs(real t,
-                               real[] y,
-                               real[] theta,
-                               real[] x_r,
-                               int[] x_i) {
-    real dydt[1];
-    real ka = theta[1]; // Dosing rate in 1/day
-    real ke = theta[2];   // Elimination rate in 1/day
-    real D = x_r[1];
-    real Cl = x_r[2];
-    real elim = ke * y[1];
-    
-    dydt[1] = 0.5*D*ke*ka*exp(-ka*t)/Cl  - elim;
-    
-    for( i in 1:8){
-      
-      dydt[1] = dydt[1] + (0.5*D*ke*ka*exp(-ka*(t-12*i))/Cl)*step(t-12*i);
-      
-    }
 
-    
-
-    return dydt;
-  }
-}
 data{
   int N; //Total number of observations
   int subjectids[N]; //Subject idendification number as an integer.  Mine go from 1 - 36
@@ -33,6 +8,7 @@ data{
   
   vector[n_subjects] sex;
   vector[n_subjects] weight;
+  vector[n_subjects] creatinine;
   vector[n_subjects] D;
 }
 parameters{
@@ -45,7 +21,7 @@ parameters{
   real<lower=0> s_t;
   vector[n_subjects] z_t;
   
-  vector<lower=0, upper=1>[n_subjects] alpha ;
+  
   
   real<lower=0, upper=1> phi;
   real<lower=0, upper=1> kappa;
@@ -53,18 +29,23 @@ parameters{
   
   real<lower=0> sigma;
   
-  real<lower=0, upper=1> alpha_a;
-  real<lower=0, upper=1> alpha_b;
+  real mu_alpha;
+  real<lower=0> s_alpha;
+  vector[n_subjects] z_alpha;
   
-  real beta_sex;
-  real beta_weight;
+  real beta_cl_sex;
+  real beta_cl_weight;
+  real beta_cl_creatinine;
+  
+  real beta_t_weight;
   
 }
 transformed parameters{
-  vector<lower=0>[n_subjects] Cl = exp(mu_cl + z_cl*s_cl + beta_sex*sex + beta_weight*weight);
-  vector<lower=0>[n_subjects] t = exp(mu_tmax + z_t*s_t);
+  vector<lower=0>[n_subjects] Cl = exp(mu_cl + z_cl*s_cl + beta_cl_sex*sex + beta_cl_weight*weight + beta_cl_creatinine*creatinine);
+  vector<lower=0>[n_subjects] t = exp(mu_tmax + z_t*s_t + beta_t_weight*weight);
+  vector<lower=0, upper=1>[n_subjects] alpha = inv_logit(mu_alpha + s_alpha*z_alpha);
   vector<lower=0>[n_subjects] ka = log(alpha)./(t .* (alpha-1));
-  vector<lower=0>[n_subjects] ke = alpha .* ka;
+  vector<lower=0>[n_subjects] ke = alpha .* log(alpha)./(t .* (alpha-1));
   vector<lower=0>[N] delayed_time = time - 0.5*delays[subjectids];
   
   vector<lower=0>[N] C = (0.5*D[subjectids] ./ Cl[subjectids]) .* (ke[subjectids] .* ka[subjectids]) ./ (ke[subjectids] - ka[subjectids]) .* (exp(-ka[subjectids] .* delayed_time) -exp(-ke[subjectids] .* delayed_time));
@@ -78,35 +59,20 @@ model{
   s_cl ~ gamma(15,100);
   z_cl ~ normal(0,1);
   
-  alpha_a ~ beta(1,1);
-  alpha_b ~ beta(5,5);
-  alpha ~ beta(alpha_a/alpha_b, (1-alpha_a)/alpha_b);
+  
+  mu_alpha ~ normal(0,1);
+  s_alpha ~ gamma(10, 100);
+  z_alpha ~ normal(0,1);
   
   phi ~ beta(20,20);
   kappa ~ beta(20,20);
   delays ~ beta(phi/kappa, (1-phi)/kappa);
   
-  beta_sex ~ student_t(3,0,2.5);
-  beta_weight ~ student_t(3,0,2.5);
+  beta_cl_sex ~ student_t(3,0,2.5);
+  beta_cl_weight ~ student_t(3,0,2.5);
+  beta_cl_creatinine ~ student_t(3,0,2.5);
+  beta_t_weight ~ student_t(3,0,2.5);
   
   sigma ~ lognormal(log(0.1), 0.2);
   yobs ~ lognormal(log(C), sigma);
-}
-generated quantities{
-  real popcl = exp(mu_cl);
-  real popa = alpha_a;
-  real poptmax = exp(mu_tmax);
-  real popka = log(popa)/(poptmax*(popa-1));
-  real popke = popa*popka;
-  real Crepeat[8,1];
-  
-  {
-    real theta[2] = {popka, popke};
-    real x_r[2] = {1, popcl};
-    int x_i[0];
-    Crepeat = integrate_ode_bdf(one_comp_lin_elim_abs, {0.0}, 0.0, {12.0,24.0,36.0,48.0,60.0,72.0,84.0,96.0}, theta, x_r, x_i);
-  }
-  
-  
-  
 }
