e5db777d66d1e4f6b0101b3c3b784cfcc386ed92
Demetri <dpananos@gmail.com>
2020-09-23 10:23:08 -0400

    errors

--- analysis/02_prepare_data.R
+++ analysis/02_prepare_data.R
@@ -5,30 +5,40 @@ library(tidymodels)
 library(posterior)
 library(Metrics)
 
+# load in the data from step 00.
+# Will have a fixed effect for study so convert it to a numeric.
+# Rescale concentrations to be mg/L since priors are on L scale and Drug amount is on mg
 concentration_data <- read_csv("data/combined_data.csv") %>% 
   mutate(
     sex = if_else(sex == 'male', 0, 1),
-    yobs = yobs/1000, #Convert from ng/ml to mg/L
+    yobs = yobs/1000,
     study = if_else(study=='old',0,1)
   ) %>% 
   rename(from_new = study)
 
 
+# Splice out new and old subjects.  Will do vfold_cv on new and append old subjects to the training set.
+# Need the old subjects since they will be used to estimate variance of random effects.
 new_subjects<-filter(concentration_data, from_new==1)
 old_subjects<-filter(concentration_data, from_new==0)
 
 
-
-
-bayes_fit_and_predict<-function(splits){
+bayes_fit_and_predict<-function(splits, filename){
   
-  #Compute how to center the predictors
-  train<-old_subjects %>% 
-         bind_rows(analysis(splits)) %>% 
+  # splits is an rsample split
+  # extract training and just append the old subjects to it.
+  # Convert the subject ids to a factor for use in compose_data eventually
+  # Converting it to a factor before causes headaches because not all levels are present in the factor.
+  train<-analysis(splits) %>% 
+         bind_rows(old_subjects) %>% 
          mutate(subjectids = factor(subjectids))
   
+  # Since old subjects appear multiple times, we cant let them affect the covariate centering.
+  # Drop all repeated people.  Keep one observation.
   unique_subjects<-distinct(train, subjectids, .keep_all = T)
   
+  
+  # Compute mean and sds of predictors where neccesary.
   covar_mean<- unique_subjects %>% 
                 select(age, creatinine, weight) %>% 
                 summarise_all(mean) %>% 
@@ -36,14 +46,17 @@ bayes_fit_and_predict<-function(splits){
   
   
   covar_sd<- unique_subjects %>% 
-    select(age, creatinine, weight) %>% 
-    summarise_all(sd) %>% 
-    as.numeric
+            select(age, creatinine, weight) %>% 
+            summarise_all(sd) %>% 
+            as.numeric
   
+  # Now, rescale the training data.
   train[,c('age','creatinine','weight')] = scale(train[,c('age','creatinine','weight')], center = covar_mean, scale = covar_sd)
   
+  # Turn the data into a list to pass to cmdstanr
   training_data_list <- compose_data(train)
   
+  # Now, we do the same for the test set.
   test<-assessment(splits) %>% 
         mutate(subjectids = factor(subjectids))
   
@@ -54,23 +67,42 @@ bayes_fit_and_predict<-function(splits){
   testing_data_list['test_n'] <- testing_data_list$n
   testing_data_list <- within(testing_data_list, rm(n))
   
-  
+  # Combine the training and test set to pass to Stan.  Fir the model, extract predictions, compute OOS error.
   combined_data<-c(training_data_list, testing_data_list)
   model<- cmdstan_model(stan_file = 'models/big_model.stan')
   fit <- model$sample(combined_data, chains = 4, parallel_chains = 4) 
   
-  draws = as_draws_df(fit$draws())
-  
+  # Only extract CPRED.  Can speed things up a little.
+  draws = as_draws_df(fit$draws('CPRED'))
   
   predictions<-draws %>% 
-    spread_draws(CPRED[i]) %>% 
-    mean_qi
+              spread_draws(CPRED[i]) %>% 
+              mean_qi
+  
   predictions$yobs<-combined_data$test_yobs
 
   error = rmse(predictions$CPRED, predictions$yobs)
   
-  return(error)
+  # Stan often causes R to crash for some reason.
+  # In case R crashes (fuck me, right?) I'll just write the output to a text file
+  fl = paste0('data/bayes_error/',filename,'.txt')
+  con = file(fl)
+  writeLines(as.character(error), con)
+  close(con)
 }
 
-
-
+linmod_fit_and_predict<-function(splits){
+  
+  training = analysis(splits)
+  val = assessment(splits)
+  
+  linmod <- lm(log(yobs) ~ time + sex + age + weight + creatinine + D , data = training)
+  
+  lm_predictions<-predict(linmod)
+  sigma = Metrics::rmse(log(training$yobs), lm_predictions)
+  lm_predictions<-exp(predict(linmod, newdata = val))*exp(sigma^2/2)
+  
+  error = rmse(lm_predictions, val$yobs)
+  
+  return(NA)
+}
--- repeated_cv.R
+++ repeated_cv.R
@@ -1,50 +1,21 @@
-library(cmdstanr)
-library(posterior)
-library(tidybayes)
 library(tidyverse)
-library(patchwork)
-library(table1)
-library(lme4)
-source("analysis/02_prepare_data.R")
+library(tidymodels)
+source('analysis/02_prepare_data.R')
+`%notin%` <- Negate(`%in%`)
 
-do_it <-function(){
-    
-  CHAINS = 4
-  
-  data<-make_split(100)
-  model = cmdstan_model(stan_file = 'models/big_model.stan')
-  fit <- model$sample(data$model_data, chains = CHAINS, parallel_chains = 4)
-  
-  
-  
-  draws = as_draws_df(fit$draws('CPRED'))
-  
-  predictions<-draws %>% 
-    spread_draws(CPRED[i]) %>% 
-    mean_qi
-  predictions$yobs<-combined_data$test_yobs
-  
-  a = Metrics::rmse(predictions$CPRED, predictions$yobs)
-  
-  linmod_data <- data$train_data %>% filter(from_old==1)
-  linmod_test_data <- data$test_data
-  colnames(linmod_test_data) = str_replace(colnames(linmod_test_data), 'test_','')
-  
-  linmod <- lm(log(yobs) ~ time + sex + age + weight + creatinine + D , data = linmod_data)
-  
-  lm_predictions<-predict(linmod)
-  sigma = Metrics::rmse( log(linmod_data$yobs), lm_predictions)
-  
-  lm_predictions<-exp(predict(linmod, newdata = linmod_test_data))*exp(sigma^2/2)
-  
-  b = Metrics::rmse(lm_predictions, linmod_test_data$yobs)
-  
-  tibble(bayes_error = a, linmod_error = b)
-  
-}
+#Get the folds I've already done up to this point
+flnames<-list.files('data/bayes_error/')
+
+#Remove the extension
+last_computed<-sub('\\.txt', '', flnames )
+
+# Create folds. Seed ensures reproducibility
+set.seed(0)
+folds<-vfold_cv(new_subjects, v = 10, repeats = 100 ) %>% 
+       mutate(filename = str_c(id, id2, sep = "_"))
 
 
-
-results<-rerun(1000, do_it()) %>% 
-          map_dfr(~ .x)
-  
+# If R crashed, just start from the last point
+folds %>% 
+  filter(filename %notin% last_computed) %>% 
+  mutate(bayes_rmse = map2(splits, filename, bayes_fit_and_predict))
